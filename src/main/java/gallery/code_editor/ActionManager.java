e691d3d294b479574de0d4b8d9a271fa59d27def
Alke-14 <kevinalbertohernandezgarcia@gmail.com>
2024-10-04 11:21:06 -0500

    added 2 java classes for TextState and Undo/Redo btns. Additional changes to GUI class

---
+++ src/main/java/gallery/code_editor/ActionManager.java
@@ -0,0 +1,13 @@
+package gallery.code_editor;
+// manages history of actions in textField
+// should connect with TextEditorState as it has the main state
+// history, and current text
+public class ActionManager {
+
+    public void UndoAction() {
+        // stack for undo
+    }
+    public void RedoAction() {
+        // stack for redo
+    }
+}
---
+++ src/main/java/gallery/code_editor/TextEditorState.java
@@ -0,0 +1,14 @@
+package gallery.code_editor;
+
+import java.awt.*;
+
+public class TextEditorState {
+
+//    public char textState(String text) {
+//        for (int i = 0; i < text.length(); i++) {
+//            char newState = text.charAt(i);
+//            System.out.println(newState);
+//        }
+//        return 0;
+//    };
+}
